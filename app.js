474b6273ab46773eb8f92267acc700cb1675c5fd
Alejandro Pascuale <alejandropascuale@MBPdeAlejandro.fibertel.com.ar>
2021-06-21 20:45:01 -0300

    creacion de rutas predeterminadas y accesos a las vistas

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
-const mainController = require('./controllers/mainController.js')
+const path = require('path');
+const publicFolder = path.resolve(__dirname, './public');
+app.use(express.static(publicFolder));
+const mainController = require('./controllers/mainController')
 
 app.listen('3001', () => {
     console.log('Servidor funcionando en el puerto 3001');
 })
 
 app.use('/', mainController)
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+})
