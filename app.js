a17b830f8ef4389ed8e7422bd337937b90e558ba
Alejandro Pascuale <alejandropascuale@MacBook-Pro-de-Alejandro.local>
2021-06-22 08:39:51 -0300

    web operativa, el problema estaba en el main controller porque no requeria el path

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ const app = express();
 const path = require('path');
 const publicFolder = path.resolve(__dirname, './public');
 app.use(express.static(publicFolder));
-const mainController = require('./controllers/mainController')
 
-app.listen('3001', () => {
+const mainRouter = require('./routes/main')
+
+
+app.listen(3001, () => {
     console.log('Servidor funcionando en el puerto 3001');
 })
 
-app.use('/', mainController)
-
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'))
-})
+app.use('/', mainRouter);
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,8 +1,13 @@
-const express = require('express');
-const router = express.Router();
+const path = require('path');
 
 
+const mainController = {
+    index: function (req, res) {
+        res.sendFile(path.resolve(__dirname, '../views/home.html'))
+    },
+    about: function (req, res) {
+        res.sendFile(path.resolve(__dirname, '../views/about.html'))
+    }
 
-
-
-module.exports = router;
+}
+module.exports = mainController;
